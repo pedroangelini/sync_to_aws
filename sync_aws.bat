0316de24b5e15a44f7e1e12231fabbb3cf53c69f
pedroangelini <72536471+pedroangelini@users.noreply.github.com>
2024-08-12 12:04:32 +0200

    add exit codes, comment dry run option, fixed bad quotes in error message

--- sync_aws.bat
+++ sync_aws.bat
@@ -16,16 +16,18 @@ IF "%AWS_SECRET_ACCESS_KEY%"=="" GOTO err
 :: all checks complete, let's sync
 echo === new session == from %from%, to %to% >> sync_log.log
 echo start rclone sync from %from%, to %to% >> sync_log.log
+::rclone sync -v --dry-run --log-file=sync_log.log %from% %to%
 rclone sync -v --log-file=sync_log.log %from% %to% 
 echo completed ok
 GOTO end
 
 :err
-echo "usage: sync_aws.bat <from> <to>"
-echo "expects to have a .env file containing the AWS key and secret in the format:"
-echo "AWS_ACCESS_KEY=..." 
-echo "AWS_SECRET_ACCESS_KEY=..."
+echo usage: sync_aws.bat <from> <to>
+echo expects to have a .env file containing the AWS key and secret in the format:
+echo AWS_ACCESS_KEY=...
+echo AWS_SECRET_ACCESS_KEY=...
+exit /b 1
 
 :end
-
 endlocal
+exit /b 0
